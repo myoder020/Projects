3133dbf0b8dda8494fa76ea2809a9187e09bc0d7
myoder020 <myoder020@gmail.com>
2014-03-26 10:32:09 -0500

    added buttons and listeners to GeoQuiz

--- android/GeoQuiz/src/com/example/GeoQuiz/QuizActivity.java
+++ android/GeoQuiz/src/com/example/GeoQuiz/QuizActivity.java
@@ -2,14 +2,50 @@ package com.example.GeoQuiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_quiz);
+
+        mTrueButton = (Button)findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this,
+                        R.string.correct_toast,
+                        Toast.LENGTH_SHORT).show();
+            }
+
+        });
+        mFalseButton = (Button)findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this,
+                        R.string.incorrect_toast,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_quiz, menu);
+        return true;
+    }*/
 }
