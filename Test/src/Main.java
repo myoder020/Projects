c255d8dc6d6ed54635d385698a2b2eb80b29bac8
Matthew Yoder <myoder020@gmail.com>
2015-05-25 15:09:16 -0500

    adding Test dir from intelliJ ide

---
+++ Test/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Yoder on 5/25/2015.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        new MyFile();
+    }
+
+}
---
+++ Test/src/MyFile.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class MyFile {
+    
+    public MyFile() {
+        System.out.println("Hello this is myfile");
+    }
+    
+}
