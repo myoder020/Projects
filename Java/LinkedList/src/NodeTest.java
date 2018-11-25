5c3bdd70cc2c4ddad27446018c26d0ce61049fab
myoder020 <myoder020@gmail.com>
2018-11-24 19:02:46 -0600

    initial checkin 11/24/2018

--- Java/LinkedList/src/NodeTest.java
+++ Java/LinkedList/src/NodeTest.java
@@ -10,43 +10,9 @@ public class NodeTest {
         Node head = null;
         int[] num = new int[20];
 
-        /*
+        System.out.println(myNode.getPerson().getName());
+
         myNode.setNext(nextNode);
-
-        Node current;
-
-        current = myNode;
-
-        Person temp;
-
-        while(current != null) {
-            temp = current.getPerson();
-            System.out.println(temp.getName());
-            current = current.getNext();
-        }
-
-        head = Node.Insert(head, 5);
-        for(int i=0; i < 20; i++) {
-            Node.Insert(head, i);
-        }
-
-
-        Node current = head;
-        while(current != null) {
-            System.out.println(current.getData());
-            current = current.getNext();
-        }
-*/
-        String s1 = "Java";
-        String s2 = "Java";
-        String s3 = "java";
-        StringBuilder sb1 = new StringBuilder();
-        sb1.append("Ja").append("va");
-        System.out.println(s1 == s2);
-        System.out.println(s1.equals(s2));
-        System.out.println(sb1.toString() == s1);
-        System.out.println(sb1.toString().equals(s1));
-        System.out.println(s1.toLowerCase() == s3.toLowerCase());
-        System.out.println(s1.toLowerCase().equals(s3.toLowerCase()));
+        System.out.println(myNode.getNext().getPerson().getName());
     }
 }
