fe2a5b1a09aa74455e330ce332e9d1f8b48f3b8d
myoder020 <myoder020@gmail.com>
2018-12-04 18:28:50 -0600

    working version of getting all directories in a folder

--- webDev/DisplayPictures/getDir.php
+++ webDev/DisplayPictures/getDir.php
@@ -1,66 +1,83 @@
 <?php 
     $dir=dir('M:\Pictures');
-    $tar = array();
-    $aar = checkContents($dir, $tar);
-    $_SESSION['dirs'] = json_encode($aar);
-    
+    $aar = checkContents($dir);
+    $_SESSION['dirs'] = $aar;
     $dir->close();
-    // echo $aar[array_search('2017',array_column($aar,'dirName'))]['dirPath'].DIRECTORY_SEPARATOR.$aar[array_search('PlantProject',array_column($aar,'dirName'))]['files'][0];
 
-    function checkContents($d, $t) {
+    function checkContents($d) {
         $nar = array(); 
         $tempA = array();
-        // echo "In checkContents function";  
+        $finalArray = array();
+
         echo nl2br("\r\n");
         while(($fn = $d->read()) !== false) {
             if($fn === "." || $fn === ".." || $fn[0] === ".")
                 continue;
-            
-            // if(is_file($d->path.DIRECTORY_SEPARATOR.$fn)) {
-            //     array_push($t, $fn);
-            // }
 
             if(is_dir($d->path.DIRECTORY_SEPARATOR.$fn)) {
                $nar = makeDirArray($fn, $d->path); 
-               array_push($t, $nar);              
+               array_push($tempA, $nar);              
             }
 
         }
-        return $t;
+
+        $pattern = '/\\\\/';
+
+        foreach($tempA as $dir) {
+            $cnt = count($dir);
+            for($i=0;$i<$cnt;$i++) {
+                if(isset($dir[$i])) {
+                    preg_match_all($pattern,$dir[$i]['dirPath'], $matches, PREG_OFFSET_CAPTURE);
+                    $dir[$i]['dirName'] = substr($dir[$i]['dirPath'],$matches[0][1][1]);
+                    array_push($finalArray, $dir[$i]);
+                    unset($dir[$i]);
+                }
+            }  
+            array_push($finalArray, $dir);          
+        }
+
+        print_r($finalArray);
+        return $finalArray;
     }
 
     function getFiles($path) {
         $far = array();
+        $temp = array();
         $files = scandir($path);
+        $cnt = 0;
+
         foreach($files as $file) {
-   
-            // if(is_file($path.DIRECTORY_SEPARATOR.$file)) {
-            //     array_push($far, $file);
-            // }
 
             if(is_dir($path.DIRECTORY_SEPARATOR.$file) && $file !== "." && $file !== "..") {
-                $dar = makeDirArray($file, $path);
-                array_push($far, $dar);   
+                $cnt++;
+                $far = makeDirArray($file, $path);  
+                array_push($temp, $far);             
             }         
 
         }
-        return $far;
+
+        if($cnt > 0) {
+            return $temp;
+        } else {
+            return null;
+        }
     }
 
     function makeDirArray ($f, $p) {
+        $final = array();
         $dar = array(
             ('dirName') => $f,
-            ('dirPath') => $p.DIRECTORY_SEPARATOR.$f,
-            ('files') => getFiles($p.DIRECTORY_SEPARATOR.$f)
+            ('dirPath') => $p.DIRECTORY_SEPARATOR.$f
         );
-        return $dar;
+        $final = $dar;
+        $sub = getFiles($p.DIRECTORY_SEPARATOR.$f);
+        if($sub !== null) {
+            foreach($sub as $sb) {
+                array_push($final, $sb);
+            }
+        } 
+        return $final;
     }
 
-    function displayPictures($p) {
-        
-        echo $p;
-    }
-
-    
     ?>
 
--- webDev/DisplayPictures/index.php
+++ webDev/DisplayPictures/index.php
@@ -3,9 +3,11 @@ session_start();
 include("nav.html");
 include("getDir.php");
 $dirs = $_SESSION['dirs'];
+// print_r($dirs);
 ?>
 
-<script type="text/javascript">
-var parr = '<?php echo $dirs ?>';
-document.write(parr[0]);
-;</script>
+<!-- <script type="text/javascript">
+// var parr = '';
+// nparr = JSON.stringify(parr);
+// alert(nparr);
+</script> -->
