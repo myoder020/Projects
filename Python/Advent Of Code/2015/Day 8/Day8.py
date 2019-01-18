e1f2f37789e3825bc2451786b7d55db6a2ab0ed6
myoder020 <myoder020@gmail.com>
2019-01-17 18:03:40 -0600

    finished AOC Day 8 2015

--- Python/Advent Of Code/2015/Day 8/Day8.py
+++ Python/Advent Of Code/2015/Day 8/Day8.py
@@ -5,31 +5,43 @@ with open('8.txt') as fh:
 # bPattern = r'\\'
 hexPattern = r'\\x..|\\\\|\\"'
 res = {}
+totalStringLiterals = 0
+totalHexChars = 0
+totalNStringLiterals = 0
 
 for line in fileContents:
+    totalStringLiterals += len(line)
     totalCnt = 0
     hexChars = 0
-    # bSearch = re.search(bPattern,i)
-    hexSearch = re.findall(hexPattern,line)
+    nline = ""
 
-    # if bSearch:
-    #     print(i)
-    #     print(bSearch)
+    hexSearch = re.findall(hexPattern,line)
 
     for i in hexSearch:
         if i.startswith("\\x"):
             hexChars += 3
         else:
             hexChars += 1
-
-    print(line, len(line), hexChars, (len(line) - hexChars - 2))
+        
+    totalHexChars += (len(line) - hexChars - 2)
     totalCnt = len(line) - (len(line) - hexChars - 2)
     res[line] = totalCnt
-    print(totalCnt)
+    nline = line.replace('\\','\\\\')
+    nline = '"'+nline.replace('\"','\\"')+'"'
+    totalNStringLiterals += len(nline)
+    print(line, nline)
+    
+
 
 total = 0
 
 for k,v in res.items():
+    # print(k, v)
     total += v
 
 print(total)
+
+print(totalStringLiterals-totalHexChars)
+print(totalNStringLiterals-totalStringLiterals)
+
+# print(res.items())
